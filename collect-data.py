8d6c0e47dac7f8c2be68b47fb75afa4d06821519
RitabrataDas343 <dasritabrata5@gmail.com>
2021-10-21 17:26:59 +0530

    [UPDATE] collect data

--- collect-data.py
+++ collect-data.py
@@ -24,6 +24,8 @@ while True:
     _, frame = cap.read()
     frame = cv2.flip(frame, 1)
     
+    cv2.putText(frame, "Expressando - TDOC 2021", (175, 450), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (225,255,255), 3)
+
     count = {'zero': len(os.listdir(directory+"/0")),
              'one': len(os.listdir(directory+"/1")),
              'two': len(os.listdir(directory+"/2")),
@@ -31,33 +33,31 @@ while True:
              'four': len(os.listdir(directory+"/4")),
              'five': len(os.listdir(directory+"/5"))}
     
-    cv2.putText(frame, "MODE : "+mode, (10, 50), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (0,0,255), 1)
-    cv2.putText(frame, "IMAGE COUNT", (10, 100), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (0,0,255), 1)
-    cv2.putText(frame, "ZERO : "+str(count['zero']), (10, 120), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (0,0,255), 1)
-    cv2.putText(frame, "ONE : "+str(count['one']), (10, 140), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (0,0,255), 1)
-    cv2.putText(frame, "TWO : "+str(count['two']), (10, 160), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (0,0,255), 1)
-    cv2.putText(frame, "THREE : "+str(count['three']), (10, 180), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (0,0,255), 1)
-    cv2.putText(frame, "FOUR : "+str(count['four']), (10, 200), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (0,0,255), 1)
-    cv2.putText(frame, "FIVE : "+str(count['five']), (10, 220), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (0,0,255), 1)
+    cv2.putText(frame, "MODE : "+mode, (10, 50), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (225,255,255), 1)
+    cv2.putText(frame, "IMAGE COUNT", (10, 100), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (225,255,255), 1)
+    cv2.putText(frame, "ZERO : "+str(count['zero']), (10, 120), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (255,255,255), 1)
+    cv2.putText(frame, "ONE : "+str(count['one']), (10, 140), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (255,255,255), 1)
+    cv2.putText(frame, "TWO : "+str(count['two']), (10, 160), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (255,255,255), 1)
+    cv2.putText(frame, "THREE : "+str(count['three']), (10, 180), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (255,255,255), 1)
+    cv2.putText(frame, "FOUR : "+str(count['four']), (10, 200), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (255,255,255), 1)
+    cv2.putText(frame, "FIVE : "+str(count['five']), (10, 220), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (255,255,255), 1)
     
     
     x1 = int(0.5*frame.shape[1])
     y1 = 10
     x2 = frame.shape[1]-10
     y2 = int(0.5*frame.shape[1])
-    cv2.rectangle(frame, (x1-1, y1-1), (x2+1, y2+1), (255,0,0) ,1)
+    cv2.rectangle(frame, (x1-1, y1-1), (x2+1, y2+1), (255,0,0) ,3)
     roi = frame[y1:y2, x1:x2]
     roi = cv2.resize(roi, (200, 200)) 
- 
+    cv2.putText(frame, "R.O.I", (440, 350), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (0,225,0), 3)
     cv2.imshow("Frame", frame)
     
-    #_, mask = cv2.threshold(mask, 200, 255, cv2.THRESH_BINARY)
-    #kernel = np.ones((1, 1), np.uint8)
-    #img = cv2.dilate(mask, kernel, iterations=1)
-    #img = cv2.erode(mask, kernel, iterations=1)
     roi = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
     _, roi = cv2.threshold(roi, 120, 255, cv2.THRESH_BINARY)
     cv2.imshow("ROI", roi)
+
+
     
     interrupt = cv2.waitKey(10)
     if interrupt & 0xFF == 27:
